a1a5adbbf3f70f6312bf667940d9e8fb778f0296
jsalazarv <jsalazarv8@gmail.com>
2024-05-22 10:56:29 -0600

    Added deps and configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,18 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
     },
-    settings: {
-        react: {
-            version: 'detect'
-        },
-    },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'standard',
-        'eslint-config-prettier',
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: [
-        'react'
-    ],
-    rules: {
-    }
-}
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcss from './postcss.config.cjs';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   const path = process.env.PROJECT_PATH;
 
   return {
+    css: {
+      postcss,
+    },
     plugins: [react()],
-    base: path ? path : '/',
+    base: path || '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@modules': '/src/modules',
+        '@layouts': '/src/layouts',
+        '@assets': '/src/assets',
+        '@common': '/src/common',
+        '@utils': '/src/common/utils',
+      },
+    },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
   };
 });
